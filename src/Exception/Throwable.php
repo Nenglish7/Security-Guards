b87b6822cdf1e2233d9d458623232ecc6e8b10c5
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-03-25 01:12:41 -0400

    Create Throwable.php

---
+++ src/Exception/Throwable.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Celestial Body.
+ *
+ * @author  Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/Nenglish7/composer-engine> Github repository.
+ * @license <https://github.com/Nenglish7/composer-engine/blob/master/LICENSE> GPL-3.0 License.
+ *
+ * @package composer-engine.
+ */
+ 
+namespace Nenglish7/ComposerEngine/Exception;
+
+/**
+ * Throwable.
+ */
+interface Throwable
+{
+    public function getMessage();
+    public function getCode();
+    public function getFile();
+    public function getLine();
+    public function getTrace();
+    public function getTraceAsString();
+    public function getPrevious();
+    public function __toString();
+}
