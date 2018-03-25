e678a079ec5cd500d78b8b0f34797b10f84f1ade
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-03-25 01:17:20 -0400

    Add invalid argument exception.

---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Composer Engine.
+ *
+ * @author  Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/Nenglish7/composer-engine> Github repository.
+ * @license <https://github.com/Nenglish7/composer-engine/blob/master/LICENSE> GPL-3.0 License.
+ *
+ * @package composer-engine.
+ */
+ 
+namespace Nenglish7\ComposerEngine\Exception;
+
+/**
+ * InvalidArgumentException.
+ */
+class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
+{
+}
