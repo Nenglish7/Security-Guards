5f954c328d4a997084291f12b749ff08d0eda770
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-03-26 17:38:40 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -10,27 +10,13 @@
  * @package composer-engine.
  */
 
-use Nenglish7\ComposerEngine\Options;
+/**
+ * use Nenglish7\ComposerEngine\Process;
+ */
 use Nenglish7\ComposerEngine\Process;
-use Nenglish7\ComposerEngine\InvalidArgumentException as Nenglish7InvalidArgumentException;
-use Nenglish7\ComposerEngine\UnexpectedValueException as Nenglish7UnexpectedValueException;
 
 function composer_install($options = [])
 {
-    if (is_array($options) || $options instanceof Traversable) {
-        foreach ($options as $option) {
-            if (!in_array($option, Options::INSTALL, true)) {
-                throw new Nenglish7UnexpectedValueException(sprintf(
-                    'The `%s` option is not recognized. Allowed: `%s`.',
-                    htmlspecialchars($option, ENT_QUOTES),
-                    serialize(Options::INSTALL)
-                ));
-            }
-        }
-        $process = new Process($options, 'install');
-        $process->run();
-    }
-    throw new Nenglish7InvalidArgumentException(
-        'The variable `$options` is not an array or an instance of `Traversable`.'
-    );
+    $process = new Process($options, 'install');
+    $process->run();
 }
