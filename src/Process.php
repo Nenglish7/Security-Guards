dbb02f9ffcdeb3319f7339948c055cc33395a577
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-03-26 11:36:24 -0400

    Update Process.php

--- src/Process.php
+++ src/Process.php
@@ -17,4 +17,77 @@ namespace Nenglish7\ComposerEngine;
  */
 class Process implements ProcessInterface
 {
+    /**
+     * @var array $commandTypes The list of command types.
+     */
+    private $commandTypes = [
+        'install'
+    ];
+    
+    /**
+     * @var string $commandType The command type passed.
+     */
+    private $commanType = '';
+    
+    /**
+     * @var array $options The list of options passed.
+     */
+    private $options = [];
+    
+    /**
+     * Process the requested command.
+     *
+     * @param array $options     The requested options for the command.
+     * @param string $commanType The requested command type.
+     *
+     * @throws InvalidArgumentException If the command type is not recognized.
+     *
+     * @return void
+     */
+    public function __construct($options = [], $commandType)
+    {
+        if (!\in_array($commandType, $this->commandTypes, \true)) {
+            throw new Exception\InvalidArgumentException(\sprintf(
+                'The variable `$command` needs to be a string. Passed: `%s`.',
+                \gettype($commandType)
+            ));
+        }
+        if (is_array($options) || $options instanceof Traversable) {
+            foreach ($options as $option) {
+                if (!in_array($option, Options::INSTALL, true)) {
+                    throw new Nenglish7UnexpectedValueException(sprintf(
+                        'The `%s` option is not recognized. Allowed: `%s`.',
+                        htmlspecialchars($option, ENT_QUOTES),
+                        serialize(Options::INSTALL)
+                    ));
+                }
+            }
+        } else {
+            throw new Exception\InvalidArgumentException(
+                'The variable `$options` is not an array or an instance of `Traversable`.'
+            );
+        }
+        $this->commanType = \escapeshellarg($commandType);
+        $this->options = $options;
+    }
+    
+    /**
+     * Execute the command.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $optionLine = ' ';
+        foreach ($this->options as $option) {
+            $optionLine .= \escapeshellarg($option) . ' ';
+        }
+        $optionLine = rtrim($optionLine);
+        if ($this->commanType == 'install') {
+            try {
+                exec('composer install' . $optionLine);
+            }
+        }
+        return true;
+    }
 }
