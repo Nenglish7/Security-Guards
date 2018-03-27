167ac7367504efe33144e9a742b74fef2cf957c8
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-03-27 16:55:58 -0400

    Add some tests.

--- tests/ProcessTest.php
+++ tests/ProcessTest.php
@@ -1,1 +1,68 @@
+<?php
+/**
+ * Composer Engine.
+ *
+ * @author  Nicholas English <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/Nenglish7/composer-engine> Github repository.
+ * @license <https://github.com/Nenglish7/composer-engine/blob/master/LICENSE> GPL-3.0 License.
+ *
+ * @package composer-engine.
+ */
 
+namespace Nenglish7\ComposerEngine;
+
+/**
+ * use PHPUnit\Framework\TestCase;
+ */
+use PHPUnit\Framework\TestCase;
+
+/**
+ * ProcessTest.
+ */
+class ProcessTest extends TestCase
+{
+    // {{
+        // {{
+    public function testConstructor()
+    {
+        $process1 = new Process();
+        $process2 = new Process();
+        $process3 = new Process();
+        $process4 = new Process();
+        $this->assertTrue((bool) $process1);
+        $this->assertTrue((bool) $process2);
+        $this->assertTrue((bool) $process3);
+        $this->assertTrue((bool) $process4);
+    }
+    // }}
+    // {{
+    public function testConstructorExceptionErrors1()
+    {
+        $this->expectException(Exception\InvalidArgumentException::class);
+        $process1 = new Process('foo');
+    }
+    public function testConstructorExceptionErrors2()
+    {
+        $this->expectException(Exception\InvalidArgumentException::class);
+        $process1 = new Process(\true);
+    }
+    // }}
+    // {{
+    public function testConstructorExceptionErrors3()
+    {
+        $this->expectException(Exception\InvalidArgumentException::class);
+        $process1 = new Process('update', 'foo');
+    }
+    public function testConstructorExceptionErrors4()
+    {
+        $this->expectException(Exception\InvalidArgumentException::class);
+        $process1 = new Process('update', 'bar');
+    }
+    // }}
+    // }}
+    // {{
+    // {{
+    // }}
+    // }}
+}
